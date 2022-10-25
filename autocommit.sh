907533a46e670dd71fc0b62442a98c152a61e5f6
tincx3D <tincx3d@gmail.com>
2022-10-25 16:10:14 +0200

    Autocommit from 2022-10-25 16:10:14

--- autocommit.sh
+++ autocommit.sh
@@ -47,11 +47,11 @@ grab_version(){
 
 push_config(){
   cd $config_folder
-  git pull
+  #git pull
   git add .
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push
+  git push origin main
 }
 
 #grab_version
